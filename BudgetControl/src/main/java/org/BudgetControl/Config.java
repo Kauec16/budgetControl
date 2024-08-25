d36e2f5f1e8c9e9ecb619265ba28db8be21980f5
Kauec16 <Kauec16>
2024-08-25 02:15:45 -0300

    Inicio da estrutura do projeto

---
+++ BudgetControl/src/main/java/org/BudgetControl/Config.java
@@ -0,0 +1,14 @@
+package org.BudgetControl;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+
+@Configurable
+public class Config {
+
+    @Bean(name = "applicationName")
+    public String applicationName(){
+        return "BudgetControll";
+    }
+
+}
---
+++ BudgetControl/src/main/java/org/BudgetControl/admApplication.java
@@ -0,0 +1,11 @@
+package org.BudgetControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class admApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(admApplication.class, args);
+    }
+}
